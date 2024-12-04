f06556520445774716f02fbe21edb3add60edd11
CSS <rahamathgousiya@gmail.com>
2024-12-04 10:26:47 +0530

    my work

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Footer =()=>{
+    return (
+        <footer style={{padding:'5px',background:'RGB(35,102,123)',color:'white',textAlign:'center',marginTop:'410px'}}>
+            <p>&copy;2024 My App, All rights reserved.</p>
+        </footer>
+    );
+};
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header =() => {
+    return(
+        <header style={{padding:'10px',background:'RGB(35,102,123)',color:'white',textAlign:'center'}}>
+            <h1>MY Header</h1>
+        </header>
+    );
+}
+export default Header;
